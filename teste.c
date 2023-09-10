f38455d426da3422fa2da5dde8d51e327210b5f2
MauricioL88 <lisboa.sse@gmail.com>
2023-09-09 23:09:17 -0300

    Primeiro commit, salvando programa teste em C

---
+++ teste.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+
+
+
+int main() {	
+
+	setlocale(LC_ALL, "portuguese");
+	
+	printf("Executando o programa em linguagem C!\n");
+	printf("Escrito por Maurício Silva Lisboa\n\n");
+	
+	system("pause");
+	return 0;
+}
